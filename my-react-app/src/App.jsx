48118643068e9a16155d2d8ac748a6859f71914d
Minulck <miulck@gmail.com>
2025-02-15 10:25:41 +0530

    linked the contact us page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Component/Header/Header";
 import HomePage from "./pages/HomePage";
 import Translator from "./Component/Translator/Translator";
+import ContactInfo from "./Component/ContactInfo/contactinfo";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/translator" element={<Translator/>} />
+          <Route path="/contactus" element={<ContactInfo/>} />
         </Routes>
       </div>
     </Router>
