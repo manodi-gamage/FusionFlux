9c264198f5f9030b7c7d1a2a20df6e4a85029491
VindhyaKulasinghe <vindyakulasinghe@gmail.com>
2025-02-15 10:10:37 +0530

    gd

--- my-react-app/src/Component/Header/Header.jsx
+++ my-react-app/src/Component/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
           <li><a href="#home">Home</a></li>
           <li><a href="#sri-lanka-tours">Sri Lanka Tours</a></li>
           <li><a href="#hotels">Hotels</a></li>
-          <li><a href="#sports-tours">Sports Tours</a></li>
+          <li><a href="#sports-tours">Sporsts Tours</a></li>
           <li><a href="#medical-holidays">Medical Holidays</a></li>
           <li><a href="#about-us">About Us</a></li>
           <li><a href="#contact-us">Contact Us</a></li>
